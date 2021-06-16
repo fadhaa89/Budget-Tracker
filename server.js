2963088a442dc830db4d8133163be1401bffa22e
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-06-15 21:29:19 -0500

    new installion and updates in service-worker/.env

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const redText = '\x1b[31m%s\x1b[0m';
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_ENDPOINT =
+  process.env.MONGODB_ENDPOINT ||
+  console.log(redText, 'You must have a Mongo Database endpoint.');
 
 const app = express();
 
@@ -16,11 +21,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_ENDPOINT, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false ,
+  useUnifiedTopology: true
 });
-
+mongoose.connection.on('connected', () =>
+  console.log('Connected to MongoDB Endpoint')
+);
+mongoose.connection.on('error', (err) =>
+  console.log(redText, `Mongoose default connection error: ${err}`)
+);
 // routes
 app.use(require("./routes/api.js"));
 
